427a34a9114f4df671f3a5f9afdec1da93855dcc
Gaël Poupard <gael+github@ffoodd.fr>
2023-07-26 00:13:44 +0200

    feat(PW.2023): fully working SVG generator for conferences

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,49 @@
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setUseGitIgnore(false)
+	eleventyConfig.setUseGitIgnore(false)
 
-  eleventyConfig.addPassthroughCopy('_site/static')
-  eleventyConfig.addWatchTarget('_site/static/docs.css')
-  eleventyConfig.addWatchTarget('_site/static/docs.js')
+	eleventyConfig.addFilter('date', (dateObj) => {
+		return DateTime.fromISO(dateObj).setLocale('fr').toLocaleString(DateTime.DATE_FULL)
+	})
 
-  eleventyConfig.setBrowserSyncConfig({
-    ui: false,
-    ghostMode: false,
-    open: true,
-    browser: 'firefox',
-  })
+	// Paris Web 2023 en SVG
+	eleventyConfig.addFilter('wrap', (title) => {
+		const lines = title.split('\n')
+		const wrappedTitle = lines.map((line, index) => {
+			let offsets = []
+			// @note Comment simplifier ça ?
+			switch (lines.length) {
+				case 3:
+					offsets = [314,400,486]
+					break;
+				case 2:
+					offsets = [400,486]
+					break;
+				case 1:
+				default:
+					offsets = [486]
+					break;
+			}
+			return `<tspan x="120" y="${offsets[index]}">${line}</tspan>`;
+		})
+		return wrappedTitle.join('')
+	})
 
-  return {
-    dir: {
-      input: '_site',
-      output: 'docs'
-    },
-    markdownTemplateEngine: 'njk'
-  }
+	eleventyConfig.addPassthroughCopy("_site/img")
+
+	eleventyConfig.setBrowserSyncConfig({
+		ui: false,
+		ghostMode: false,
+		open: true,
+		browser: 'firefox',
+	})
+
+	return {
+		dir: {
+			input: '_site',
+			output: 'docs'
+		},
+		markdownTemplateEngine: 'njk'
+	}
 }
